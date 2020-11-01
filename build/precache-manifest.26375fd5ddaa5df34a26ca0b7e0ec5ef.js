4a8f262c0fb0c073f5f41c1532e581b500b2d36d
Ievgenii <bijibnui88@gmail.com>
2020-11-01 14:16:39 +0200

    index.html

---
+++ build/precache-manifest.26375fd5ddaa5df34a26ca0b7e0ec5ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107b3e6c8ad265f055039b9a43737a70",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "242c631582916b0b5845",
+    "url": "/goit-react-hw-02-feedback/static/css/main.3c41ab7f.chunk.css"
+  },
+  {
+    "revision": "31e5e02ac5713c47dfc8",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c0c4f5a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c0c4f5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "242c631582916b0b5845",
+    "url": "/goit-react-hw-02-feedback/static/js/main.b10f3237.chunk.js"
+  },
+  {
+    "revision": "a89455118c4cd01451f3",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.66612386.js"
+  }
+]);
